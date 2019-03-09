b5470da9b8bff59a55197587c7badeb0772e4208
杨柳 <144844097541@qq.com>
2019-03-09 11:41:33 +0800

    two

--- team5/src/pages/home/home.jsx
+++ team5/src/pages/home/home.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import {NavLink} from 'react-router-dom';
 import { connect } from 'dva';
 import "./css/home.css"
-import { Layout, Menu } from 'element-react';
+import { Layout, Menu ,Button } from 'element-react';
 import 'element-theme-default';
 import { Route} from 'dva/router';
 import TestList from '../testList'
@@ -23,8 +23,13 @@ import Look from '../look'
                 <header>
                     <p className="left">网站工程</p>
                     <div className="right">
-                        <p className="center"><img src="" alt=""/></p>
-                        <p className="">用户名</p>
+                        <p className="center">
+                        亲爱的<b>cabh</b>
+                        你的身份是<b>nksa;v</b>
+                        </p>
+                    </div>
+                    <div>
+                    <Button>退出登录</Button>
                     </div>
                 
                 </header>
@@ -35,17 +40,28 @@ import Look from '../look'
                                 <Menu defaultActive="2" className="el-menu-vertical-demo" onOpen={this.onOpen.bind(this)} onClose={this.onClose.bind(this)}>
                                     <Menu.SubMenu index="1" title={<span><i className="el-icon-message"></i>考试管理</span>}>
                                         <Menu.Item index="1-1">
-                                            <NavLink to="/home/testList">添加试题</NavLink>
+                                            <NavLink to="/home/add">添加试题</NavLink>
                                         </Menu.Item>
                                         <Menu.Item index="1-2">
-                                            <NavLink to="/home/add">试题分类</NavLink>
+                                            <NavLink to="/home/testList">试题分类</NavLink>
                                         </Menu.Item>
                                         <Menu.Item index="1-3">
                                             <NavLink to="/home/look">查看试题</NavLink>
                                         </Menu.Item>
                                     </Menu.SubMenu>
-                                    <Menu.Item index="2"><i className="el-icon-menu"></i>用户管理</Menu.Item>
-                                    <Menu.Item index="3"><i className="el-icon-setting"></i>班级管理</Menu.Item>
+                                    <Menu.SubMenu index="2" title={<span><i className="el-icon-plus"></i>用户管理</span>}>
+                                        <Menu.Item index="2-1">
+                                            <NavLink to="/home/testList">添加用户</NavLink>
+                                        </Menu.Item>
+                                        <Menu.Item index="2-2">
+                                            <NavLink to="/home/add">用户展示</NavLink>
+                                        </Menu.Item>
+                                    </Menu.SubMenu>
+                                    <Menu.SubMenu index="3" title={<span><i className="el-icon-setting"></i>班级管理</span>}>
+                                        <Menu.Item index="3-1">
+                                            <NavLink to="/home/testList">添加试题</NavLink>
+                                        </Menu.Item>
+                                    </Menu.SubMenu>
                                 </Menu>
                             </Layout.Col>
                         </Layout.Row>
